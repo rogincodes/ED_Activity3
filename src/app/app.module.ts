caf27f632a34cf9146fbc0ac1c912e5522cb300b
Rogin Cartalla <tetsudrew@gmail.com>
2021-10-24 20:18:34 +0800

    added all codes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,52 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+
+const routes: Routes = [
+  {path: "home", component: HomeComponent,
+    children: [
+      {path: "profile", component: ProfileComponent},
+      {path: "gallery", component: GalleryComponent},
+      {path: "contact", component: ContactComponent},  
+    ]
+  },
+  {path: "**", redirectTo: "home"}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    GalleryComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      routes
+    ),
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatCardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
